beea3d5c275a8d35b3f648f3424824760d1336bb
Petr Vorel <petr.vorel@gmail.com>
2022-07-26 19:17:54 +0200

    package/debianutils: bump version to 5.7-0.2
    debian/copyright has been moved to a different tarball
    (debianutils_5.7-0.2.debian.tar.xz) which does not contain the source
    code, thus download from salsa git instead of
    https://snapshot.debian.org/.
    
    Add patch to disable po4a (unnecessary dependency just to get
    translations).
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- package/debianutils/debianutils.mk
+++ package/debianutils/debianutils.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-DEBIANUTILS_VERSION = 4.11
-DEBIANUTILS_SOURCE = debianutils_$(DEBIANUTILS_VERSION).tar.xz
-DEBIANUTILS_SITE = http://snapshot.debian.org/archive/debian/20200525T145753Z/pool/main/d/debianutils
+DEBIANUTILS_VERSION = 5.7-0.2
+DEBIANUTILS_SOURCE = debianutils-debian-$(DEBIANUTILS_VERSION).tar.bz2
+DEBIANUTILS_SITE = https://salsa.debian.org/debian/debianutils/-/archive/debian/$(DEBIANUTILS_VERSION)
+DEBIANUTILS_AUTORECONF = YES
 DEBIANUTILS_CONF_OPTS = --exec-prefix=/
 DEBIANUTILS_LICENSE = GPL-2.0+, SMAIL (savelog)
 DEBIANUTILS_LICENSE_FILES = debian/copyright
